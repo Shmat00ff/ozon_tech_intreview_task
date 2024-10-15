251001da79cc39da3fd2cc80950865340eb7a270
shmat00ff <shmat00ff@gmail.com>
2024-10-15 19:14:52 +0700

    add init params, response err process

--- test_dogs.py
+++ test_dogs.py
@@ -1,22 +1,59 @@
 import random
-
 import pytest
 import requests
+import os
+import logging
+from retrying import retry
+
+#Делаем логи, для простоты отслеживания событий
+logging.basicConfig(level=logging.INFO)
+
+#Скрыть токен, для безопасности
+TOKEN = os.getenv('YANDEX_DISK_TOKEN')
+
 
 class YaUploader:
-    def __init__(self):
-        pass
+    def __init__(self, token):
+        #Добавим основные параметры, которые будут повторяться
+        self.token = token
+        self.base_url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': f'OAuth {token}'
+        }
 
-    def create_folder(self, path, token):
-        url_create = 'https://cloud-api.yandex.net/v1/disk/resources'
-        headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
-        response = requests.put(f'{url_create}?path={path}', headers = headers)
+    #Добавим обработку ошибок HTTP-запросов.
+    def create_folder(self, path):
+        """
+        Создаёт папку на Яндекс Диске по указанному пути.
+        """
 
-    def upload_photos_to_yd(self, token, path, url_file, name):
-        url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
-        params = {"path": f'/{path}/{name}', 'url': url_file, "overwrite": "true"}
-        resp = requests.post(url, headers=headers, params=params)
+        try:
+            response = requests.put(f'{self.base_url}?path={path}', headers=self.headers)
+            response.raise_for_status()
+            logging.info(f'Папка "{path}" успешно создана.')
+        except requests.exceptions.HTTPError as err:
+            logging.error(f'Ошибка при создании папки: {err}')
+            return False
+        return True
+
+    #Добавим декоратор для исключения ошибок
+    @retry()
+    def upload_photos_to_yd(self, path, url_file, name):
+        """
+        Загружает файл по URL на Яндекс.Диск.
+        """
+        try:
+            upload_url = f"{self.base_url}/upload"
+            params = {"path": f'/{path}/{name}', 'url': url_file, "overwrite": "true"}
+            response = requests.post(upload_url, headers=self.headers, params=params)
+            response.raise_for_status()
+            logging.info(f'Файл "{name}" загружен в папку "{path}".')
+        except requests.exceptions.HTTPError as err:
+            logging.error(f'Ошибка при загрузке файла "{name}": {err}')
+            return False
+        return True
 
 
 def get_sub_breeds(breed):
